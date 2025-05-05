f767859a561cb65316584bf557da9601c316f3e5
CosLynx AI <drixisgod@gmail.com>
2025-05-06 03:59:55 +0530

    generated file: `src/App.tsx` [base-template, ui-components]

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '@/styles/index.css'
+import '@/styles/tailwind.css'
+import Home from '@/pages/Home'
+
+function App() {
+  return (
+    <React.StrictMode>
+      <Home />
+    </React.StrictMode>
+  )
+}
+
+export default App
